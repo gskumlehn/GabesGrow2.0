ee92c67ccfb6c9511ccf0e82b6131c9c06d82ac5
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-12 16:35:47 -0300

    Update de hosts

--- setup/settings.py
+++ setup/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7r8bg^*!o2^agw5l6_oqrg+24s6qa#e08#^p!povm-s-y6e-s-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '192.168.15.2']
 
 
 # Application definition
