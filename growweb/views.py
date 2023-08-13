85b102f6630b9d5fbdc64d24c38a8270669235b8
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-13 15:19:15 -0300

    Cria a pasta de templates e renderiza index.html

--- growweb/views.py
+++ growweb/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1>Gabe`s Grow 2.0</h1>')
+    return render(request, 'index.html')
