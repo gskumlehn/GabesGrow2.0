d327590a7f2f85e85925d230ae52a42e785d4379
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-16 14:37:06 -0300

    Cria modelos de dados do air, solo, config do grow, e histórico de mudanças

--- growweb/models.py
+++ growweb/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class AirData(models.Model):
+    date = models.DateTimeField(null=False, blank=False)
+    temperature = models.DecimalField(null=False, blank=False, max_digits=4, decimal_places=2)
+    humidity = models.DecimalField(null=False, blank=False, max_digits=5, decimal_places=2)
+
+class SoilData(models.Model):
+    date = models.DateTimeField(null=False, blank=False)
+    humidity = models.DecimalField(null=False, blank=False, max_digits=5, decimal_places=2)
+
+class GrowConfig(models.Model):
+    lastUpdate = models.DateTimeField(null=False, blank=False)
+    stage = models.CharField(max_length=10, null=False, blank=False)
+    watering = models.BooleanField(null=False, blank=False)
+class GrowConfigHistory(models.Model):
+    growConfig = models.ForeignKey("GrowConfig", on_delete=models.CASCADE)
