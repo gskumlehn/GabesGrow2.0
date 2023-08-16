80d62b4c5ff00bdc997df18272bbb3ca27f47d19
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-16 19:51:06 -0300

    Adiciona temperatura

--- growweb/views.py
+++ growweb/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'growweb/index.html')
+    temperature = 22.00
+    return render(request, 'growweb/index.html', {temperature: temperature})
 def data(request):
     return render(request, 'growweb/data.html')
