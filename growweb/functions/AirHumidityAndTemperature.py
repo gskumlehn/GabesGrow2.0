340fe876cb8ff21972147691398f422ab510e96f
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-17 10:46:34 -0300

    Corrige metodo

--- growweb/functions/AirHumidityAndTemperature.py
+++ growweb/functions/AirHumidityAndTemperature.py
@@ -1,7 +1,9 @@
 import Adafruit_DHT
-class AirHumidityAndTemperature:
-    global DHT_SENSOR = Adafruit_DHT.DHT22
-    global DHT_PIN = 4
 
-    def get():
-        return Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+DHT_SENSOR = Adafruit_DHT.DHT22
+DHT_PIN = 4
+
+def get():
+    return Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+
+
