e4886e54167090933aac3509ad642c17c89e0a86
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-16 21:39:40 -0300

    Corrige uso de temperature

--- growweb/views.py
+++ growweb/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def index(request):
     temperature = 22.00
-    return render(request, 'growweb/index.html', {temperature: temperature})
+    return render(request, 'growweb/index.html', {"temperature": temperature})
 def data(request):
     return render(request, 'growweb/data.html')
