b2b9ae7ca15998cda95ea5c0c84936d8f04afff5
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-16 22:27:25 -0300

    Corrige definição

--- growweb/functions/AirHumidityAndTemperature.py
+++ growweb/functions/AirHumidityAndTemperature.py
@@ -1,7 +1,7 @@
 import Adafruit_DHT
 class AirHumidityAndTemperature:
-    DHT_SENSOR = Adafruit_DHT.DHT22
-    DHT_PIN = 4
+    global DHT_SENSOR = Adafruit_DHT.DHT22
+    global DHT_PIN = 4
 
     def get():
         return Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
