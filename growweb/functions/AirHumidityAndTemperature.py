8ca53168dca5f3e44881219e9d5c6e1651f5a431
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-17 12:16:10 -0300

    Arredonda valores

--- growweb/functions/AirHumidityAndTemperature.py
+++ growweb/functions/AirHumidityAndTemperature.py
@@ -3,7 +3,16 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-def get():
-    return Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+def gety():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    return round(humidity, 2), round(temperature, 2)
+
+def getHumidity():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    return round(humidity, 2)
+
+def getTemperature():
+    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    return round(temperature, 2)
 
 
