785525e508ce5f1172c2a1b7ef9b84010365e72b
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-17 15:08:50 -0300

    Segrega apps

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,9 @@
 from datetime import datetime
 from django.shortcuts import render
+from data.functions import AirHumidityAndTemperature
 
 def index(request):
-    humidity, temperature = 70, 22
+    humidity, temperature = AirHumidityAndTemperature.get()
     collectionTime = datetime.now()
     return render(request, 'dashboard/index.html',
                   {"humidity": humidity, "temperature": temperature, "time": collectionTime})
