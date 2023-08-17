ba46d45e6f2062550cc67112cdce5c24d29828d8
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-17 12:16:57 -0300

    Corrige nome do metodo

--- growweb/functions/AirHumidityAndTemperature.py
+++ growweb/functions/AirHumidityAndTemperature.py
@@ -3,7 +3,7 @@ import Adafruit_DHT
 DHT_SENSOR = Adafruit_DHT.DHT22
 DHT_PIN = 4
 
-def gety():
+def get():
     humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
     return round(humidity, 2), round(temperature, 2)
 
