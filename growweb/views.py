c8bea04a2b829d6aaf310ecc7e494048a5b0b667
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-16 22:21:17 -0300

    Adiciona dados na tela

--- growweb/views.py
+++ growweb/views.py
@@ -1,7 +1,10 @@
+from datetime import datetime
 from django.shortcuts import render
+from growweb.functions.AirHumidityAndTemperature import AirHumidityAndTemperature
 
 def index(request):
-    temperature = 22.00
-    return render(request, 'growweb/index.html', {"temperature": temperature})
+    humidity, temperature = AirHumidityAndTemperature.get()
+    collectionTime = datetime.datetime.now()
+    return render(request, 'growweb/index.html', {"humidity": humidity, "temperature": temperature, "time": collectionTime})
 def data(request):
     return render(request, 'growweb/data.html')
