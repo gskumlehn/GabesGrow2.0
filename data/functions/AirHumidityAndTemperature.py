124567edf01c8f8d49ad4a8312771b331116b0b3
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-18 14:06:19 -0300

    remove fruit

--- data/functions/AirHumidityAndTemperature.py
+++ data/functions/AirHumidityAndTemperature.py
@@ -1,18 +1,13 @@
-import Adafruit_DHT
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
 
 def get():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+    humidity, temperature = 70, 25
     return round(humidity, 2), round(temperature, 2)
 
 def getHumidity():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    return round(humidity, 2)
+    return round(70, 2)
 
 def getTemperature():
-    humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
-    return round(temperature, 2)
+    return round(25, 2)
 
 
