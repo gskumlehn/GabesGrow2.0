0076eb274a235ef33a9f4466c10fff7a40f86971
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-18 18:07:56 -0300

    Cria view de config

--- apps/config/views.py
+++ apps/config/views.py
@@ -3,7 +3,7 @@ from django.utils.dateparse import parse_datetime
 from apps.config.models import GrowConfig, GrowConfigHistory
 
 def index(request):
-    config = GrowConfig.objects.filter(id=1).value()[0]
+    config = GrowConfig.objects.values()[0]
     lastUpdate = parse_datetime(config.lastUpdate).strftime("%d/%m/%Y - %H:%M")
     history = GrowConfigHistory.objects.order_by("date").values()
     return render(request, 'config/show.html', {"config": config, "lastUpdate": lastUpdate, "history": history})
