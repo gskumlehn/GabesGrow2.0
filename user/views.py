0aa912ac69ea499a5c34070ca6b7609e8f3ff61d
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-18 13:57:47 -0300

    Corrige chamada de dict

--- user/views.py
+++ user/views.py
@@ -1,18 +1,16 @@
 from dashboard.views import index
-from django.contrib.auth.models import User, auth
+from django.contrib.auth.models import auth
 from django.shortcuts import render, redirect
 from user.forms import LoginForm
-import logging
 
 def login(request):
     form = LoginForm()
-    logger = logging.getLogger("mylogger")
-    logger.info('ooooooooooooooooooi')
+
     if request.method == 'POST':
         if form.is_valid():
 
-            username = form.username.value()
-            password = form.password.value()
+            username = form['username'].value()
+            password = form['password'].value()
 
             user = auth.authenticate(request, username=username, password=password)
 
