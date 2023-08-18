e7390cda713be74f06a926422f4392c1c5859bbd
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-18 18:01:31 -0300

    Cria view de config

--- apps/config/views.py
+++ apps/config/views.py
@@ -3,5 +3,6 @@ from apps.config.models import GrowConfig, GrowConfigHistory
 
 def index(request):
     config = GrowConfig.objects.filter(id=1)
+    lastUpdate = config.lastUpdate.strftime("%d/%m/%Y - %H:%M")
     history = GrowConfigHistory.objects.order_by("date").values()
-    return render(request, 'config/show.html', {"config": config, "history": history})
+    return render(request, 'config/show.html', {"config": config, "lastUpdate": lastUpdate, "history": history})
