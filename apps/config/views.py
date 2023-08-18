c2f91b77214013a323fe9c060f711171472227e0
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-18 17:59:05 -0300

    Cria view de config

--- apps/config/views.py
+++ apps/config/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from apps.config.models import GrowConfig, GrowConfigHistory
 
 def index(request):
-    return render(request, 'config/show.html')
+    config = GrowConfig.objects.filter(id=1)
+    history = GrowConfigHistory.objects.orderBy("date").values()
+    return render(request, 'config/show.html', {"config": config, "history": history})
