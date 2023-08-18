3714316115d802e22919b8151185ee6633571e46
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-17 22:36:10 -0300

    Converte para lista

--- stage/views.py
+++ stage/views.py
@@ -5,5 +5,5 @@ def show(request):
     return render(request, 'stage/show.html')
 
 def list(request):
-    stages = Stage.objects.all()
+    stages = list(Stage.objects.all())
     return render(request, 'stage/list.html', {"stages": stages})
