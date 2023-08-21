55b9e2eeb87aa1b833f8b0b72c1fc63e87bb67ef
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-21 18:03:54 -0300

    Filtra corretamente

--- apps/config/views.py
+++ apps/config/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from apps.config.models import GrowConfig, GrowConfigHistory
 
 def index(request, config_id):
-    config = GrowConfig.objects.get(config_id)
+    config = GrowConfig.objects.get(id=config_id)
     history = GrowConfigHistory.objects.order_by("date").values()
     return render(request, 'config/show.html', {"config": config, "history": history})
