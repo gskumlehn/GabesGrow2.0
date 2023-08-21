fc556e32c0b8f0366f07e20c3a8dbe9fbf93d5ee
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-21 18:01:37 -0300

    Remiove esapco

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from apps.config.views import index
 
 urlpatterns = [
-    path('<int: config_id>', index, name='config'),
+    path('<int:config_id>', index, name='config'),
 ]
