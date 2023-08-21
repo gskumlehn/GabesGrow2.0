44db4e0ab92f4a365f1e73d1908bd254aad47815
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-21 18:11:37 -0300

    Filtra corretamente

--- apps/config/views.py
+++ apps/config/views.py
@@ -3,5 +3,5 @@ from apps.config.models import GrowConfig, GrowConfigHistory
 
 def index(request, config_id):
     config = GrowConfig.objects.get(id=config_id)
-    history = GrowConfigHistory.objects.order_by("date").values()
+    history = GrowConfigHistory.objects.filter(id=config_id).order_by("date").values()
     return render(request, 'config/show.html', {"config": config, "history": history})
