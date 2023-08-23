1fe8201f1654b70db0a288a7459b1517b80f61ca
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-22 21:46:31 -0300

    Altera form de stage

--- apps/config/urls.py
+++ apps/config/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from apps.config.views import index
+from apps.config.views import index, edit
 
 urlpatterns = [
     path('<int:config_id>', index, name='config'),
+    path('edit/<int:config_id>', edit, name='editConfig'),
 ]
