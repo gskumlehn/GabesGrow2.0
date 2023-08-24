65298113a758876106318758663660d6924c3c83
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-24 16:36:04 -0300

    creates data collection scheduler

--- apps/data/apps.py
+++ apps/data/apps.py
@@ -1,10 +1,10 @@
 from django.apps import AppConfig
-from apps.data.scheduler import DataCollectionScheduler
 
 class DataConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.data'
 
     def ready(self):
+        from apps.data.scheduler import DataCollectionScheduler
         DataCollectionScheduler.start()
 
