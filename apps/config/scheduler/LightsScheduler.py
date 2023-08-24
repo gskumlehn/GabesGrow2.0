3500af85ec9acb388d2577b92e482b8fd54d583c
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-24 19:32:25 -0300

    fix job

--- apps/config/scheduler/LightsScheduler.py
+++ apps/config/scheduler/LightsScheduler.py
@@ -2,22 +2,24 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apps.config.functions.Lights import lightsOnIfVegetative, lightsOffIfVegetative, lightsOnIfNotVegetative, lightsOffIfNotVegetative
 def start():
     scheduler = BackgroundScheduler()
-    # scheduler.add_job(func=lightsOnIfVegetative,
-    #                   trigger="cron",
-    #                   minute=0,
-    #                   hour=6)
+    scheduler.add_job(func=lightsOnIfVegetative,
+                      trigger="cron",
+                      minute=30,
+                      hour=4)
 
     scheduler.add_job(func=lightsOffIfVegetative,
                       trigger="cron",
-                      minute=29,
-                      hour=19)
-    # scheduler.add_job(func=lightsOnIfNotVegetative,
-    #                   trigger="cron",
-    #                   minute=30,
-    #                   hour=4)
-    #
-    # scheduler.add_job(func=lightsOffIfNotVegetative,
-    #                   trigger="cron",
-    #                   minute=30,
-    #                   hour=18)
+                      minute=30,
+                      hour=22)
+
+    scheduler.add_job(func=lightsOnIfNotVegetative,
+                      trigger="cron",
+                      minute=0,
+                      hour=6)
+
+    scheduler.add_job(func=lightsOffIfNotVegetative,
+                      trigger="cron",
+                      minute=0,
+                      hour=18)
+
     scheduler.start()
