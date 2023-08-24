48955fce61d8c6bef266ff74405de349b04e5087
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-24 17:30:57 -0300

    creates data collection scheduler

--- apps/data/scheduler/DataCollectionScheduler.py
+++ apps/data/scheduler/DataCollectionScheduler.py
@@ -6,6 +6,6 @@ def start():
     scheduler = BackgroundScheduler()
     scheduler.add_job(func=AirData.objects.create(humidity=getHumidity(), temperature=getTemperature()).save,
                       trigger="cron",
-                      minutes="0/30",
+                      minute="0/30",
                       replace_existing=True)
     scheduler.start()
