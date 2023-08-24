70239a3e4a3da25ae5f0f6ba7fe9a0ea71daff45
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-24 18:57:32 -0300

    fix job

--- setup/settings.py
+++ setup/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.15.1',
                  '192.168.15.2',
