ee0c65df0abe24565e834d96b45604678691e9f9
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-24 16:38:15 -0300

    creates data collection scheduler

--- apps/config/scheduler/lightScheduler.py
+++
@@ -1,7 +0,0 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from apps.config.models import GrowConfig
-
-def start():
-    scheduler = BackgroundScheduler
-    config = GrowConfig.objects.get(id=1)
-    scheduler.addJob(config.lightOn, "interval")
