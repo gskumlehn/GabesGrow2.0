d5d849e0886f670fd8093e9ee5c4cf25c26b32a8
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-25 10:31:59 -0300

    Adds djangoscheduler

--- apps/config/scheduler/WateringScheduler.py
+++ apps/config/scheduler/WateringScheduler.py
@@ -3,7 +3,7 @@ from apps.config.functions import Watering
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(func=Watering.waterIfDry(),
+    scheduler.add_job(func=Watering.waterIfDry,
                       trigger="interval",
                       minutes=30,
                       jobstore='djangojobstore')
