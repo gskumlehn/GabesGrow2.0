b736b563923096721f02440b67abc4f9b1a18a00
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-25 11:14:15 -0300

    test

--- apps/data/functions/DataCollection.py
+++ apps/data/functions/DataCollection.py
@@ -4,6 +4,7 @@ from datetime import datetime
 
 def collectDataIfNecessary():
     data = AirData.objects.all()[0]
-    now = datetime.now()
-    if data.date.minute != now.minute:
-        AirData.objects.create(humidity=getHumidity(), temperature=getTemperature()).save()
+    if data is not None:
+        now = datetime.now()
+        if data.date.minute != now.minute:
+            AirData.objects.create(humidity=getHumidity(), temperature=getTemperature()).save()
