cce854e6e7f91e9c6b57553cf617b2884916c67d
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-25 11:32:20 -0300

    test

--- apps/data/functions/DataCollection.py
+++ apps/data/functions/DataCollection.py
@@ -1,9 +1,9 @@
 from apps.data.functions.AirHumidityAndTemperature import getHumidity, getTemperature
 from apps.data.models import AirData
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def collectDataIfNecessary():
-    start = datetime.now - datetime.timedelta(minutes=15)
+    start = datetime.now - timedelta(minutes=15)
     end = datetime.now
     dataList = AirData.objects.filter(date__range=[start, end])
     if not dataList:
