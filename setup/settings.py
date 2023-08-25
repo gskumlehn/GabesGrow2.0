f6e40494388a3902add7e258be811d024888bf39
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-25 09:44:28 -0300

    Adds djangoscheduler

--- setup/settings.py
+++ setup/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'apps.data.apps.DataConfig',
     'apps.stage.apps.StageConfig',
     'apps.user.apps.UserConfig',
-    'django_apscheduler'
+    'django.apscheduler'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
