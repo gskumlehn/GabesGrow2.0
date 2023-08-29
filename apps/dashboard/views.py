9f26afc35f3e20b474e22311ae678ef41a13ff08
gabrielkumlehn <gabriel.kumlehn@asaas.com.br>
2023-08-29 09:48:17 -0300

    test

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -11,7 +11,7 @@ def index(request):
     humidity, temperature = AirHumidityAndTemperature.get()
     dry = isDry()
     collectionTime = datetime.now().strftime("%H:%M")
-    return render(request, 'dashboard/index.html',{"humidity": humidity, "temperature": temperature, "wet": wet, "time": collectionTime})
+    return render(request, 'dashboard/index.html',{"humidity": humidity, "temperature": temperature, "dry": dry, "time": collectionTime})
 
 def data(request):
     data = AirData.objects.all()[0]
